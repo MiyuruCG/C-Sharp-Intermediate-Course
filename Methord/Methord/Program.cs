76dc82a62b747c12ad702e5fed751ea934d72839
Miyuru <miyurugunatilake@gmail.com>
2020-05-21 19:18:40 +0530

    Using Out modifier

--- Methord/Methord/Program.cs
+++ Methord/Methord/Program.cs
@@ -6,10 +6,28 @@ namespace Methord
     {
         static void Main(string[] args)
         {
-            
+            //var number = int.Parse("abc");  /*This gives an excepsion*/
+            /*
+             * same as above (but exception handled)
+             * try
+             * {
+             *  var num = int.Parse("abc");
+             * }
+             * catch(Exception)
+             * {
+             * Console.WriteLine("Error");
+             * }
+             */
 
+            int number;
+            var result = int.TryParse("abc", out number);
 
+            if(result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversinon failed...");
         }
+
         static void UaeParams()
         {
             var cal = new Calculator();
