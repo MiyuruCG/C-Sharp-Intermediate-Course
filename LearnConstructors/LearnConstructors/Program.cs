2f360485c97dd5e1969a7fd6fbcc1c3e9af6a619
Miyuru <miyurugunatilake@gmail.com>
2020-05-21 17:48:25 +0530

    create project

---
+++ LearnConstructors/LearnConstructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LearnConstructors
+{
+    public class Customer
+    {
+        public int Id;
+        public String Name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
