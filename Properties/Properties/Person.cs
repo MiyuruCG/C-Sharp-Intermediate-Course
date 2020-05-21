69cb3397d42fe2e007d3d22ef45501949f720f8b
Miyuru <miyurugunatilake@gmail.com>
2020-05-22 00:31:00 +0530

    learn Access Modifiers

---
+++ Properties/Properties/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; private set; } //here the c# compiler creates a private variable and creates get and set methods.
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public int Age
+        {
+            get
+            { 
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
