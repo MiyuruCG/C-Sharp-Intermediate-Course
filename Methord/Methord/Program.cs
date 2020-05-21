6f2b69e8f472765a648f85d2c10f4b2071b86d62
Miyuru <miyurugunatilake@gmail.com>
2020-05-21 19:01:04 +0530

    Learn Methods - Point class

---
+++ Methord/Methord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                //point.Move(new Point(40, 60));
+                point.Move(null);
+                Console.WriteLine("Pointer is at ({0}, {1})", point.x, point.y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Pointer is at ({0}, {1})", point.x, point.y);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Unexpected error occurd");
+            }
+
+
+            
+           
+        }
+    }
+}
