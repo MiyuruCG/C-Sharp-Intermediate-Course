e06f0e5c1e775d3a86f10626b5d948b4ed201607
Miyuru <miyurugunatilake@gmail.com>
2020-05-22 18:20:27 +0530

    Constructors

---
+++ Section 4/Constructors/Constructors/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Car : Vehical
+    {
+        public Car(string registrationNo)
+            :base(registrationNo)
+        {
+            Console.WriteLine("Car is initialize...{0}",registrationNo);
+        }
+    }
+}
---
+++ Section 4/Constructors/Constructors/Vehical.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Vehical
+    {
+        private readonly string registrationNo;
+
+       /* public Vehical()
+        {
+            Console.WriteLine("vehical initialize...");
+        }*/
+
+        public Vehical(string registrationNo)
+        {
+            this.registrationNo = registrationNo;
+
+            Console.WriteLine("Vehical is being initialized {0}", registrationNo);
+        }
+    }
+}
