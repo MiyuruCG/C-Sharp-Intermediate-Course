b996cabd6ebbda7926d3075466a26eb1db838083
Miyuru <miyurugunatilake@gmail.com>
2020-05-22 22:31:03 +0530

    Boxing

---
+++ Section 4/Boxing/Boxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new ArrayList();// can add any object 
+            list.Add(1); // here boxing happes this effect the run time
+            list.Add("String");
+            list.Add(DateTime.Today);
+
+            // var number = (int)list[1]; //gets error
+
+            var anotherList = new List<int>(); // generic list
+            anotherList.Add(1);// type safe
+
+            var nameList = new List<string>();
+            nameList.Add("hi");
+        }
+    }
+}
