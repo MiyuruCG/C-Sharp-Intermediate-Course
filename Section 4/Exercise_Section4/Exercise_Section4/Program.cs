8cc4d0a14849885b4096fd22f4e16918dce1b123
Miyuru <miyurugunatilake@gmail.com>
2020-05-22 23:52:59 +0530

    exercise section4  - Stack

---
+++ Section 4/Exercise_Section4/Exercise_Section4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_Section4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            System.Console.WriteLine(stack.Pop());
+            System.Console.WriteLine(stack.Pop());
+            System.Console.WriteLine(stack.Pop());
+
+
+        }
+    }
+}
---
+++ Section 4/Exercise_Section4/Exercise_Section4/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise_Section4
+{
+    public class stack
+    {
+        private Stack stc = new Stack();
+        public stack()
+        {
+
+        }
+        
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                Console.WriteLine("Cannot publish a Null value here...");
+                throw new InvalidOperationException();
+            }
+
+            stc.Push(obj);
+        }
+
+        public object Pop()
+        {
+            object ob = stc.Pop();
+            if (ob == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return ob;
+        }
+
+        public void Clear()
+        {
+            stc.Clear();
+        }
+    }
+}
