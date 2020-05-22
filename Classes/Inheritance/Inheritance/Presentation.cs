1b40f5a8986fb446a53e1b06c38720e19017e644
Miyuru <miyurugunatilake@gmail.com>
2020-05-22 13:05:27 +0530

    Inheritance

---
+++ Classes/Inheritance/Inheritance/Presentation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Presentation
+    {
+        public int width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object duplicates ..");
+        }
+    }
+}
---
+++ Classes/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.width = 100;
+            text.Copy();
+            text.AddHyperLink("www.");
+        }
+    }
+}
