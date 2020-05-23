7f96065fe90bbc2b217e7bb8717337270d8ef25d
Miyuru <miyurugunatilake@gmail.com>
2020-05-23 23:38:36 +0530

    learn Overriding

---
+++ Section 5/MethodOverriding/MethodOverriding/Shape.cs
@@ -0,0 +1,32 @@
+﻿namespace MethodOverriding
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            //code specific for the circle class
+            System.Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            System.Console.WriteLine("Draw a rectangle");
+        }
+    }
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+
+        public virtual void Draw()
+        {
+
+        }
+
+    }
+}
